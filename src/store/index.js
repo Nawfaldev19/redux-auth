f67aeb88786de8e2b6f829a938c87938c6490022
Nawfaldev19 <108766715+Nawfaldev19@users.noreply.github.com>
2022-10-02 20:32:56 +0500

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,44 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuthentication: false };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    }, //state, action=null
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthentication = true;
+    },
+    logout(state) {
+      state.isAuthentication = false;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+export default store;
